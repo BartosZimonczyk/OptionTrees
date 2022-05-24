4d09098ab750293d9ae0f4c41bd6c46025e4feb3
BarotsZimonczyk <bartosz.zimonczyk@gmail.com>
2022-05-24 14:08:41 +0200

    More heatmaps gen scripts

--- simulations/simulating_heatmaps.R
+++ simulations/simulating_heatmaps.R
@@ -1,5 +1,6 @@
 source('src/basic_functions.R')
 
+# Time vs S0
 Times <- seq(1, 30, by=1)
 S0s <- seq(1, 100, by=1)
 
@@ -34,3 +35,39 @@ write.csv(heat_matrix_AmericaCall, "simulations/s0_vs_time_AmericaCall_heatmap.c
 
 heatmap(heat_matrix_AmericaPut, Rowv=NA, Colv=NA)
 write.csv(heat_matrix_AmericaPut, "simulations/s0_vs_time_AmerciaPut_heatmap.csv")
+
+# Strike vs S0
+Strikes <- seq(18, 78, by=1)
+S0s <- seq(1, 100, by=1)
+
+heat_matrix_EurCall <- matrix(nrow=length(S0s), ncol=length(Strikes))
+heat_matrix_EurPut <- matrix(nrow=length(S0s), ncol=length(Strikes))
+heat_matrix_AmericaCall <- matrix(nrow=length(S0s), ncol=length(Strikes))
+heat_matrix_AmericaPut <- matrix(nrow=length(S0s), ncol=length(Strikes))
+
+for(j in 1:length(Strikes)){
+  for(i in 1:length(S0s)){
+    cat(i, ',', j, '...\n')
+    EurCall <- make_option(TypeContinent = "Europe", TypeVerb = "call", Strike = Strikes[j], S0 = S0s[i])
+    EurPut <- make_option(TypeContinent = "Europe", TypeVerb = "put", Strike = Strikes[j], S0 = S0s[i])
+    AmericaCall <- make_option(TypeContinent = "America", TypeVerb = "call", Strike = Strikes[j], S0 = S0s[i])
+    AmericaPut <- make_option(TypeContinent = "America", TypeVerb = "put", Strike = Strikes[j], S0 = S0s[i])
+    
+    heat_matrix_EurCall[i, j] <- option_profit_from_tree(make_tree(EurCall))
+    heat_matrix_EurPut[i, j] <- option_profit_from_tree(make_tree(EurPut))
+    heat_matrix_AmericaCall[i, j] <- option_profit_from_tree(make_tree(AmericaCall))
+    heat_matrix_AmericaPut[i, j] <- option_profit_from_tree(make_tree(AmericaPut))
+  }
+}
+
+heatmap(heat_matrix_EurCall, Rowv=NA, Colv=NA)
+write.csv(heat_matrix_EurCall, "simulations/s0_vs_Strike_EurCall_heatmap.csv")
+
+heatmap(heat_matrix_EurPut, Rowv=NA, Colv=NA)
+write.csv(heat_matrix_EurPut, "simulations/s0_vs_Strike_EurPut_heatmap.csv")
+
+heatmap(heat_matrix_AmericaCall, Rowv=NA, Colv=NA)
+write.csv(heat_matrix_AmericaCall, "simulations/s0_vs_Strike_AmericaCall_heatmap.csv")
+
+heatmap(heat_matrix_AmericaPut, Rowv=NA, Colv=NA)
+write.csv(heat_matrix_AmericaPut, "simulations/s0_vs_Strike_AmerciaPut_heatmap.csv")
