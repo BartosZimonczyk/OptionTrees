6ba6e19935ad9f49c67713d669bc83ab92d8bee9
mabuszka <magda@buszka.eu>
2022-05-31 15:41:53 +0200

    update sensitivity plots script

--- plots/make_sensitivity_plots.R
+++ plots/make_sensitivity_plots.R
@@ -76,15 +76,29 @@ ggsave("plots/Sensetivity_plots/r.png", g5)
 
 # wrażliwość na zmianę DeltaT
 
+DeltaTs <- seq(0.01, 1, by=0.001)
+parity <- as.integer(2/DeltaTs) %%2
 
 DeltaT_price_simulation <- read.csv(file='simulations/DeltaT_price_simulation.csv', row.names = 1)
-g6 <- ggplot(DeltaT_price_simulation)+
-  geom_line(aes(x = DeltaT, y = Price, color = Continent, linetype = Verb))+
+g6 <-
+  ggplot(cbind(DeltaT_price_simulation))+
+  geom_line(aes(x = DeltaT, y = Price, linetype = Verb, color = Continent))+
+  # geom_line(aes(x = DeltaT, y = Price,linetype = Continent), alpha= 0.2, size = 0.1)+
+  # facet_grid(~Verb)+
   scale_color_discrete(labels = c("Amerykańskie", "Europejskie"))+
   labs(y = "Cena", color = "Typ:", linetype = "Typ:")+
   theme_bw()
 
 ggsave("plots/Sensetivity_plots/deltaT.png", g6)
+
+# g7 <-
+#   ggplot(cbind(DeltaT_price_simulation, parity = parity)[DeltaT_price_simulation$Continent == "America" & DeltaT_price_simulation$Verb=="put", ])+
+#   geom_point(aes(x = DeltaT, y = Price, color = as.factor(parity)), shape = 20, alpha = 1, size = 0.1)+
+#   scale_color_discrete(labels = c("Amerykańskie", "Europejskie"))+
+#   labs(y = "Cena", color = "Parzystość:")+
+#   theme_bw()
+# 
+# ggsave("plots/Sensetivity_plots/deltaT_parity.png", g7)
 
 
 # skąd sie bierze parzyste nieparzyste -> 
